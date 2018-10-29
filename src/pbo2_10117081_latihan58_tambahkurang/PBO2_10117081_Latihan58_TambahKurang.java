41057a1f4128490bd5671113bb91d620142643bc
Geo Syah Alkautsar <43498342+GeoSyah@users.noreply.github.com>
2018-10-29 22:13:10 +0700

    Add files via upload

---
+++ src/pbo2_10117081_latihan58_tambahkurang/PBO2_10117081_Latihan58_TambahKurang.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2_10117081_latihan58_tambahkurang;
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * NIM : 10117081
+ * Kelas : IF2/PBO2
+ * Deskripsi Program : Memperlihatkan hasil perjumlahan dan selisih bilangan
+ */
+public class PBO2_10117081_Latihan58_TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JumlahBilangan jubil = new JumlahBilangan();
+       jubil.tampilHasilJumlah();
+       
+       SelisihBilangan seblg = new SelisihBilangan();
+       seblg.tampilSelisih();
+    }
+    
+}
